93a57546401fc6c217f09ffc0fcbcfab25f04a3c
andsc <andsc0830900@gmail.com>
2019-04-02 14:13:10 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+// Sockets
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = {
+  url: environment.wsUrl, options: {}
+};
 
 import { AppComponent } from './app.component';
 
