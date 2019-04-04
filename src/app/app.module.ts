e5d7d2a30c51d6f09da90f494652c02199dfaacb
asaez <asaezc@sel.gfi.es>
2019-04-04 14:56:34 +0200

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { FormsModule } from '@angular/forms';
 
 // Sockets
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
@@ -10,13 +11,19 @@ const config: SocketIoConfig = {
 };
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
